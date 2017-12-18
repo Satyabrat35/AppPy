c578de0105bf6df5460cde54db36d33d5962cd99
Satyabrat35 <satyabrat35@gmail.com>
2017-12-18 11:56:53 +0530

    WTForms

--- apppy/forms.py
+++ apppy/forms.py
@@ -3,6 +3,24 @@ from wtforms import StringField,validators
 from wtforms.fields.html5 import URLField
 from wtforms.validators import DataRequired , url 
 
+
+
 class BookmarkForm(Form):
-	obj = URLField('url',validators=[DataRequired(),url()])
-	description = StringField('description')
+    obj = URLField('The URL for your bookmark:', validators=[DataRequired(),url()])
+    description = StringField('Optional description:')
+
+    def validate(self):
+        
+        if not self.obj.data.startswith("http://") or\
+            self.obj.data.startswith("https://"):
+            self.obj.data = "http://" + self.url.data
+
+        if not Form.validate(self):
+            return False
+
+        if not self.description.data:
+            self.description.data = self.obj.data + " is the URL"
+
+        return True
+
+
